502b9fe53278a87eedcbe0a013c1c3550de33702
margulan-kalykul <98729909+margulan-kalykul@users.noreply.github.com>
2024-10-11 14:07:58 +0800

    Beginning

---
+++ assignment4/Dicee/Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by user252323 on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignment4/Dices/Dices/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dices
+//
+//  Created by user252323 on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var diceImage1: UIImageView!
+    @IBOutlet var diceImage2: UIImageView!
+    
+    let diceImages = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        diceImage1.image = UIImage(named: "dice1")
+        diceImage2.image = UIImage(named: "dice1")
+    }
+
+    @IBAction func rollDices(_ sender: UIButton) {
+        let num1 = Int.random(in: 0..<diceImages.count)
+        let num2 = Int.random(in: 0..<diceImages.count)
+        
+        diceImage1.image = diceImages[num1]
+        diceImage2.image = diceImages[num2]
+    }
+}
+

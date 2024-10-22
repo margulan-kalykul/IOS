9b418b334f73db483a965caa2af449de4e59923b
margulan-kalykul <m_kalykul@kbtu.kz>
2024-10-22 22:27:17 +0800

    Base project 5

---
+++ assignment5/Xylo/Xylo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xylo
+//
+//  Created by user252323 on 10/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

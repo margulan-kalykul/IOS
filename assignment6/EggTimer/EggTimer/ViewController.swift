b394df0ee5d674efec1f10e5ea882d8c383fa2b7
margulan-kalykul <m_kalykul@kbtu.kz>
2024-10-23 13:49:11 +0800

    Base project 6

---
+++ assignment6/EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by user252323 on 10/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var pregressBar: UIProgressView!
+    @IBOutlet var softEgg: UIImageView!
+    @IBOutlet var mediumEgg: UIImageView!
+    @IBOutlet var hardEgg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func eggTapped(_ sender: UIImageView) {
+        
+    }
+
+}
+

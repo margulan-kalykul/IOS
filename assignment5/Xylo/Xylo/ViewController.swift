85a7c07a894f07f0dc343ea090ebb3bd110216cd
margulan-kalykul <m_kalykul@kbtu.kz>
2024-10-23 11:02:25 +0800

    Assignment 5

--- assignment5/Xylo/Xylo/ViewController.swift
+++ assignment5/Xylo/Xylo/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
+    // All the buttons for the notes
+    /*@IBOutlet var red: UIImageView!
+    @IBOutlet var orange: UIImageView!
+    @IBOutlet var yellow: UIImageView!
+    @IBOutlet var lightBlue: UIImageView!
+    @IBOutlet var blue: UIImageView!
+    @IBOutlet var purple: UIImageView!*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // print("Check")
+        
+    }
+    
+    func playNote(note: String) {
+        if let soundUrl = Bundle.main.url(forResource: note, withExtension: "wav") {
+            do {
+                player = try AVAudioPlayer(contentsOf: soundUrl)
+                player?.play()
+            }
+            catch {
+                print("Sound error")
+            }
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton?) {
+        //sender?.backgroundColor(UIColor(named: "whitre"))
+        // print("Button pressed")
+        if let s = sender {
+            playNote(note: s.currentTitle ?? "A")
+        }
     }
 
 
 }
-

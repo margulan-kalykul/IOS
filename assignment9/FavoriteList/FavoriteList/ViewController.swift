d1fb36ee486f4e647a6af3a8a8b89f1195b1cc9a
Student <student@Admins-MacBook-Pro.local>
2024-11-08 11:16:42 +0500

    Base project 9

---
+++ assignment9/FavoriteList/FavoriteList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteList
+//
+//  Created by Student on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
